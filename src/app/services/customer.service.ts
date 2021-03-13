d196f8473cb6bc1ae3e30e70b240103fdc361f09
mustafa cagatay yılmaz <cagatay.yilmaz@ozan.com>
2021-03-13 15:11:55 +0300

    Added customer service

---
+++ src/app/services/customer.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { CustomerResponseModel } from '../models/customerResponseModel';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CustomerService {
+
+  constructor(private httpClient:HttpClient) { }
+
+  getCustomers(){
+    return this.httpClient.get<CustomerResponseModel>('https://localhost:56426/api/Customers/getallwithdetails');
+  }
+}
